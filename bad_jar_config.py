9d0c24c2b917f16520312443d1f6705aa39cc341
RolandChristensen <rgc@pdx.edu>
2014-02-16 15:25:24 -0800

    Added script to test the configuration errors and related files.

---
+++ bad_jar_config.py
@@ -0,0 +1,24 @@
+#Path to openrocket jar file
+JAR_PATH = "/bad/path/to/OpenRocket.jar"
+# PATH to libjvm.so
+JVM = "/usr/lib/jvm/jdk-7-oracle-x64/jre/lib/amd64/server/libjvm.so"
+
+#This service
+WEBSERVICE_PORT = 5000
+
+# Main FCF
+FC_IP           = '127.0.0.1'
+FC_LISTEN_PORT  = 36000
+FC_TALK_ARM     = 35666
+FC_TALK_SERVO   = 35667
+
+# Sensor Board
+SENSOR_IP       = '127.0.0.1'
+ADIS_TX_PORT    = 35020
+MPU_TX_PORT     = 35002
+MPL_TX_PORT     = 35010
+
+# Roll Board
+ROLL_IP         = '127.0.0.1'
+ROLL_TX_PORT    = 35004
+ROLL_RX_PORT    = 35003
---
+++ bad_jvm_config.py
@@ -0,0 +1,24 @@
+#Path to openrocket jar file
+JAR_PATH = "/home/roland/git/openrocket/swing/build/jar/OpenRocket.jar"
+# PATH to libjvm.so
+JVM = "/bad/path/to/libjvm.so"
+
+#This service
+WEBSERVICE_PORT = 5000
+
+# Main FCF
+FC_IP           = '127.0.0.1'
+FC_LISTEN_PORT  = 36000
+FC_TALK_ARM     = 35666
+FC_TALK_SERVO   = 35667
+
+# Sensor Board
+SENSOR_IP       = '127.0.0.1'
+ADIS_TX_PORT    = 35020
+MPU_TX_PORT     = 35002
+MPL_TX_PORT     = 35010
+
+# Roll Board
+ROLL_IP         = '127.0.0.1'
+ROLL_TX_PORT    = 35004
+ROLL_RX_PORT    = 35003
